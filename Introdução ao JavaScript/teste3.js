03973f37a50b24e8272a6e249220bd1c6739106e
Adriellison Ferreira da Costa <50798465+adriellison@users.noreply.github.com>
2022-02-23 06:43:54 -0300

    testando funções

---
+++ Introdução ao JavaScript/teste3.js
@@ -0,0 +1,17 @@
+// function max(num1, num2){
+// 	if(num1 > num2){
+// 		return num1;
+// 	}else{
+// 		return num2;
+// 	}
+// }
+// console.log(max(1, 2));
+
+function fizzBuzz(entrada){
+	return entrada
+}
+
+// entrada
+const resultado = fizzBuzz(15);
+// saida
+console.log(resultado)
