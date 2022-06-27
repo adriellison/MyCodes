2d45ae01ebf2e0b87f7e3e1a7f811a3b0a2afdaf
adriellison <adriellisonki@gmail.com>
2022-06-27 17:29:12 -0300

    Aula 3 completa

---
+++ Trabalhando com Tipos de Referência e Valor/Aula 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+namespace Aula_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+
+            p1.Nome = "And";
+            p1.Idade = 20;
+            // p1.EnderecoPessoa = default;
+            p1.EnderecoPessoa = new Endereco()
+            {
+                Logradouro = "Rua teste",
+                Numero = 1236,
+                CEP = "62000000",
+                Cidade = "Russas"
+            };
+        }
+    }
+}
