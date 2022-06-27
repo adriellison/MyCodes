62cc4a925dd69601d4a2a3cf2650e17b31c99cba
adriellison <adriellisonki@gmail.com>
2022-06-27 17:46:46 -0300

    Aula 4 Completa

---
+++ Trabalhando com Tipos de Referência e Valor/Aula 4/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+
+namespace Aula_4
+{
+    public class Numero
+    {
+        public int N { get; set;}
+        public Numero(int n)
+            {
+                N = n;
+            }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numero a = new Numero(2);
+            Numero b = new Numero(2);
+
+            if(a.N == b.N)
+            {
+                WriteLine($"a e b são iguais");
+            }
+            else
+            {
+                WriteLine($"a e b são diferentes");
+            }
+        }
+    }
+}
